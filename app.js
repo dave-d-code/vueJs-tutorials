66fe0e5d69da6063e80c4a26bffd2f3741bf0b45
David Dorr <shishkebab101@hotmail.com>
2017-07-02 13:44:45 +0300

    upto conditionals

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ new Vue({
 		age: 25,
 		x: 0,
 		y: 0,
+		iname: '',
+		iage: '',
+		a: 0,
+		b: 0,
+		cAge: 20,
+		aa: 0,
+		bb: 0,
+		available: false,
+		nearby: false,
 	},
 	methods: {
 		greet: function(time) {
@@ -23,7 +32,36 @@ new Vue({
 		updateXY: function(event) {
 			this.x = event.offsetX;
 			this.y = event.offsetY;
-		}
+		}, 
+		clickLink: function() {
+			alert('you clicked me');
+		},
+		logName: function() {
+			console.log('you entered your name');
+		},
+		logAge: function() {
+			console.log('you entered your age');
+		},
+		addToA: function() {
+			return this.a + this.age;
+		},
+		addToB: function() {
+			return this.b + this.age;
+		},
+	},
+	computed: {
+		addToAA: function() {
+			return this.aa + this.age;
+		},
+		addToBB: function() {
+			return this.bb + this.age;
+		},
+		compClasses: function() {
+			return {
+			available: this.available,
+			nearby: this.nearby,	
+			}
+		},
 	}
 
 });
